de4ab41a9aa9659a6293fe453764a615ae251db7
jarekpc <jarekpc@o2.pl>
2022-12-11 12:18:54 +0100

    add implementation for template method chapter 1.3.3

--- src/main/java/com/example/warehouse/export/TxtExporter.java
+++ src/main/java/com/example/warehouse/export/TxtExporter.java
@@ -8,17 +8,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Exporter extends AbstractExporter {//wydzielenie do osobnych klas
+public class TxtExporter extends AbstractExporter {
 
     private static final String BORDER = "*";
     private static final String LEFT_BORDER = BORDER + " ";
     private static final String RIGHT_BORDER = " " + BORDER;
     private static final String SEPARATOR = " | ";
 
+
     private final List<Integer> widths;
+
     private final int totalWidth;
 
-    public Exporter(Report report, PrintStream out) {
+    public TxtExporter(Report report, PrintStream out) {
         super(report, out);
         this.widths = calcWidths(report);
 
@@ -26,21 +28,6 @@ public class Exporter extends AbstractExporter {//wydzielenie do osobnych klas
                 + LEFT_BORDER.length()
                 + SEPARATOR.length() * (report.getLabels().size() - 1)
                 + RIGHT_BORDER.length();
-    }
-
-    @Override
-    protected void handleLabels(PrintStream out, List<String> labels) {
-        printStrings(out, labels);
-    }
-
-    @Override
-    protected void handleRecord(PrintStream out, List<String> records, boolean first, boolean last) {
-        printStrings(out, records);
-    }
-
-    @Override
-    protected void afterLabels(PrintStream out) {
-        printBorder(out);
     }
 
     private List<Integer> calcWidths(Report report) {
@@ -58,11 +45,10 @@ public class Exporter extends AbstractExporter {//wydzielenie do osobnych klas
         return widths;
     }
 
-    private void printBorder(PrintStream out) {
-        for (int i = 0; i < totalWidth; i++) {
-            out.print(BORDER);
-        }
-        out.println();
+    @Override
+    protected void handleLabels(PrintStream out, List<String> labels) {
+        printStrings(out, labels);
+
     }
 
     private void printStrings(PrintStream out, List<String> strings) {
@@ -77,4 +63,20 @@ public class Exporter extends AbstractExporter {//wydzielenie do osobnych klas
         out.println();
     }
 
+    @Override
+    protected void handleRecord(PrintStream out, List<String> records, boolean first, boolean last) {
+        printStrings(out, records);
+    }
+
+    @Override
+    protected void beforeLabels(PrintStream out) {
+        printBorder(out);
+    }
+
+    private void printBorder(PrintStream out) {
+        for (int i = 0; i < totalWidth; i++) {
+            out.print(BORDER);
+        }
+        out.println();
+    }
 }
