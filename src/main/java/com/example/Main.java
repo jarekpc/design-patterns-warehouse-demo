336c9530b00af6139732d44d924283945af45254
jarekpc <jarekpc@o2.pl>
2022-12-13 13:32:15 +0100

    strategy pattern with aguments

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,20 +9,39 @@ import java.util.List;
 
 public class Main {
 
-    public static final int CLIENT_ID = 2;
+    public static int CLIENT_ID;
 
     public static void main(String[] args) {
         final List<String> arguments = List.of(args);
+
+        checkClientId(arguments);
+        parseClientId(arguments.get(0));
 
         ProductDao productDao = new MemoryProductDao();
         CustomerDao customerDao = new DbCustomerDao();
         InventoryDao inventoryDao = new MemoryInventoryDao(productDao);
         OrderDao orderDao = new MemoryOrderDao(productDao, customerDao);
-        Warehouse warehouse = new Warehouse(productDao,customerDao,inventoryDao,orderDao);
+        Warehouse warehouse = new Warehouse(productDao, customerDao, inventoryDao, orderDao);
 
         new Web(arguments, warehouse).run();
         new Cli(arguments, warehouse).run();
 
         System.exit(0);
+    }
+
+    private static void checkClientId(List<String> arguments) {
+        if (arguments.size() < 1) {
+            System.err.println("The client ID must be specialized");
+            System.exit(1);
+        }
+    }
+
+    private static void parseClientId(String str) {
+        try {
+            CLIENT_ID = Integer.valueOf(str);
+        } catch (NumberFormatException ex) {
+            System.err.println(String.format("Illegal client id ", ex));
+            System.exit(1);
+        }
     }
 }
