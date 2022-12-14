71df873149c397cc8e3a8379fd7acbe0dc4da770
jarekpc <jarekpc@o2.pl>
2022-12-14 12:39:02 +0100

    solid strategies send mail correct code vol7

---
+++ src/main/java/com/example/warehouse/NoReportDelivery.java
@@ -0,0 +1,15 @@
+package com.example.warehouse;
+
+import com.example.warehouse.export.ExportType;
+
+public class NoReportDelivery implements ReportDelivery {
+    @Override
+    public String getName() {
+        return "No report delivery";
+    }
+
+    @Override
+    public void deliver(Report.Type reportType, ExportType exportType, byte[] bytes) throws ReportDeliveryException {
+        // INFO: intentionally left empty.
+    }
+}
